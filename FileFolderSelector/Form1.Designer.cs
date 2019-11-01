9e4a288b6f98c36b8bbcb6d8fad45770634ce459
SBL-Warehouse <warehouse@shuttlebridge.co.uk>
2019-11-01 09:57:07 +0000

    Add project files.

---
+++ FileFolderSelector/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace FileFolderSelector
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.choof_Click = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.direc_Click = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.run_Click = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // choof_Click
+            // 
+            this.choof_Click.Location = new System.Drawing.Point(151, 89);
+            this.choof_Click.Name = "choof_Click";
+            this.choof_Click.Size = new System.Drawing.Size(75, 23);
+            this.choof_Click.TabIndex = 0;
+            this.choof_Click.Text = "Select File";
+            this.choof_Click.UseVisualStyleBackColor = true;
+            this.choof_Click.Click += new System.EventHandler(this.Choof_Click_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(351, 91);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(236, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // direc_Click
+            // 
+            this.direc_Click.Location = new System.Drawing.Point(151, 180);
+            this.direc_Click.Name = "direc_Click";
+            this.direc_Click.Size = new System.Drawing.Size(175, 23);
+            this.direc_Click.TabIndex = 2;
+            this.direc_Click.Text = "Select the Folder to Save Files";
+            this.direc_Click.UseVisualStyleBackColor = true;
+            this.direc_Click.Click += new System.EventHandler(this.Direc_Click_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(351, 182);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(236, 20);
+            this.textBox2.TabIndex = 3;
+            // 
+            // run_Click
+            // 
+            this.run_Click.Location = new System.Drawing.Point(151, 294);
+            this.run_Click.Name = "run_Click";
+            this.run_Click.Size = new System.Drawing.Size(75, 23);
+            this.run_Click.TabIndex = 4;
+            this.run_Click.Text = "Go!";
+            this.run_Click.UseVisualStyleBackColor = true;
+            this.run_Click.Click += new System.EventHandler(this.Run_Click_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.run_Click);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.direc_Click);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.choof_Click);
+            this.Name = "Form1";
+            this.Text = "DPD Scan File Creator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button choof_Click;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button direc_Click;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button run_Click;
+    }
+}
+
---
+++ FileFolderSelector/Form1.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FileFolderSelector
+{
+    public partial class Form1 : Form
+    {
+        string sSelectedFile;
+        string sSelectedFolder;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Choof_Click_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog choofdlog = new OpenFileDialog();
+            choofdlog.Filter = "Excel Files (*.xlsx)|*.xlsx";
+            choofdlog.FilterIndex = 1;
+            choofdlog.Multiselect = false;
+
+            if (choofdlog.ShowDialog() == DialogResult.OK)
+            {
+                sSelectedFile = choofdlog.FileName;
+                this.textBox1.Text = sSelectedFile;
+            }
+            else
+                sSelectedFile = string.Empty;
+        }
+
+        private void Direc_Click_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+
+            if (fbd.ShowDialog() == DialogResult.OK)
+            {
+                sSelectedFolder = fbd.SelectedPath;
+                this.textBox2.Text = sSelectedFolder;
+            }
+            else
+                sSelectedFolder = string.Empty;
+        }
+
+        private void Run_Click_Click(object sender, EventArgs e)
+        {
+            if(sSelectedFolder != string.Empty && sSelectedFile != string.Empty)
+            {
+                Microsoft.Office.Interop.Excel.Application excelApp;
+
+                string fnTarget;
+                DateTime date;
+                string numOfPcs;
+                string clientCode;
+                string awb;
+                string awbEnding;
+                //fnTarget = "\\DPD TRACKING " + numOfPcs + " pcs_" + clientCode + "_" + awbEnding + ".xlsx";
+
+                //string fileTarget = "C:\\Users\\SBL-Warehouse\\Desktop\\ExcelBatchSave_Project" + fnTarget;
+                string fileTemplate = Application.StartupPath + "\\DPD TRACKING 123 pcs_SOSO_7777_template.xlsx";
+                excelApp = new Microsoft.Office.Interop.Excel.Application();
+                Microsoft.Office.Interop.Excel.Workbook wbTarget;
+                Microsoft.Office.Interop.Excel.Worksheet sh;
+
+
+                string fn;
+                SpreadsheetLight.SLDocument ss;
+                fn = sSelectedFile;
+                ss = new SpreadsheetLight.SLDocument(fn);
+                ss.SelectWorksheet("Sheet1");
+
+                //int rt;
+                //rt = 0;
+
+                Int32 row_count;
+                row_count = ss.GetWorksheetStatistics().EndRowIndex;
+
+                for (int ri = 2; ri <= row_count; ri++)
+                {
+                    date = ss.GetCellValueAsDateTime(ri, 1);
+                    clientCode = ss.GetCellValueAsString(ri, 2);
+                    awb = ss.GetCellValueAsString(ri, 3);
+                    awbEnding = awb.Substring(awb.Length - 4);
+                    numOfPcs = ss.GetCellValueAsString(ri, 4);
+
+                    wbTarget = excelApp.Workbooks.Open(fileTemplate);
+                    sh = wbTarget.Worksheets["Sheet1"];
+                    sh.Cells[1, 1] = date;
+                    sh.Cells[1, 2] = numOfPcs + " pcs";
+                    sh.Cells[1, 5] = awb;
+
+                    fnTarget = "\\DPD TRACKING " + numOfPcs + " pcs_" + clientCode + "_" + awbEnding + ".xlsx";
+                    string fileTarget = sSelectedFolder + fnTarget;
+                    wbTarget.SaveAs(fileTarget);
+                    wbTarget.Close(true);
+
+                    excelApp.Quit();
+                }
+
+                string promptValue = Prompt.ShowDialog("Program finished successfully!", "Confirmation");
+            }
+        }
+    }
+
+
+    public static class Prompt
+    {
+        public static string ShowDialog(string text, string caption)
+        {
+            Form prompt = new Form()
+            {
+                Width = 500,
+                Height = 150,
+                FormBorderStyle = FormBorderStyle.FixedDialog,
+                Text = caption,
+                StartPosition = FormStartPosition.CenterScreen
+            };
+            Label textLabel = new Label() { Left = 50, Top = 20, Width = 400, Text = text };
+            //TextBox textBox = new TextBox() { Left = 50, Top = 50, Width = 400 };
+            Button confirmation = new Button() { Text = "OK", Left = 350, Width = 100, Top = 70, DialogResult = DialogResult.OK };
+            confirmation.Click += (sender, e) => { prompt.Close(); };
+            //prompt.Controls.Add(textBox);
+            prompt.Controls.Add(confirmation);
+            prompt.Controls.Add(textLabel);
+            prompt.AcceptButton = confirmation;
+
+            return prompt.ShowDialog() == DialogResult.OK ? "All good" : "Error";
+        }
+    }
+}
